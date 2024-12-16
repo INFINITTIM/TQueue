bcabc9c13ce83bd2a4b95aab5fc9feae946d7006
wwwti <www.tiimchernov@mail.ru>
2024-12-16 15:28:18 +0300

    Добавьте файлы проекта.

---
+++ Queue/TQueue.h
@@ -0,0 +1,207 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class TQueue
+{
+	T* pMem;
+	int mem_size;
+	int element_counter;
+	int tail;
+	int head;
+
+	T operator[](int index) const;
+public:
+	// ŒÕ—“–” “Œ–€
+	TQueue(int size);
+	TQueue(const TQueue& other_queue);
+	~TQueue();
+
+	//œ≈–≈√–”« » Œœ≈–¿“Œ–Œ¬
+	TQueue& operator=(const TQueue& other_queue);
+	bool operator==(const TQueue& other_queue) const;
+	bool operator!=(const TQueue& other_queue) const;
+	
+	//‘”Õ ÷»»
+	void push(T element);
+	T pop();
+	bool isEmpty() const;
+	bool isFull() const;
+	void clear();
+	T front() const;
+	T back() const;
+
+	//œ≈–≈√–”« ¿ ¬€¬Œƒ¿ + –≈¿À»«¿÷»ﬂ
+
+
+	friend ostream& operator<<(ostream& out, const TQueue& queue)
+	{
+		for (int i = 0; i < queue.element_counter; i++) {
+			out << queue[i] << " ";
+		}
+		return out;
+	}
+};
+
+//---------------------------------------------------------//
+//                      ŒÕ—“–” “Œ–€                        //
+//---------------------------------------------------------//
+
+template <class T>
+TQueue<T>::TQueue(int size)
+{
+	mem_size = size;
+	pMem = new T[mem_size];
+	head = 0;
+	tail = -1;
+	element_counter = 0;
+}
+
+template <class T>
+TQueue<T>::TQueue(const TQueue& other_queue)
+{
+	mem_size = other_queue.mem_size;
+	head = other_queue.head;
+	tail = other_queue.tail;
+	element_counter = other_queue.element_counter;
+	pMem = new T[mem_size];
+	if (tail >= head)
+	{
+		for (int i = head; i <= tail; i++)
+		{
+			pMem[i] = other_queue.pMem[i];
+		}
+	}
+	else
+	{
+		for (int i = head; i <= mem_size; i++)
+		{
+			pMem[i] = other_queue.pMem[i];
+		}
+		for (int i = 0; i < tail; i++)
+		{
+			pMem[i] = other_queue.pMem[i];
+		}
+	}
+}
+
+template <class T>
+TQueue<T>::~TQueue()
+{
+	delete[] pMem;
+}
+
+//---------------------------------------------------------//
+//                        Œœ≈–¿“Œ–€                        //
+//---------------------------------------------------------//
+
+template <class T>
+TQueue<T>& TQueue<T>::operator=(const TQueue& q) {
+	if (this == &q) return *this;
+	if (mem_size != q.mem_size) {
+		mem_size = q.mem_size;
+		delete[] pMem;
+		pMem = new T[mem_size];
+	}
+	element_counter = q.element_counter;
+	head = q.head;
+	tail = q.tail;
+	if (q.tail >= q.head) {
+		for (int i = 0; head + i <= tail; i++) {
+			pMem[head + i] = q.pMem[head + i];
+		}
+	}
+	else {
+		for (int i = 0; i <= tail; i++) {
+			pMem[i] = q.pMem[i];
+		}
+		for (int i = head; i < size; i++) {
+			pMem[i] = q.pMem[i];
+		}
+	}
+	return *this;
+}
+
+template <class T>
+bool TQueue<T>::operator==(const TQueue& other_queue) const
+{
+	if (mem_size != other_queue.mem_size || element_counter != other_queue.element_counter) 
+		return false;
+	if (element_counter == 0) 
+		return true;
+	for (int i = 0; i < element_counter; i++) {
+		if (pMem[(head + i) % mem_size] != other_queue.pMem[(other_queue.head + i) % mem_size]) 
+			return false;
+	}
+	return true;
+}
+
+template <class T>
+bool TQueue<T>::operator!=(const TQueue& other_queue) const
+{
+	return (!(*this == other_queue));
+}
+
+template <class T>
+T TQueue<T>::operator[](int index) const {
+	return pMem[(head + index) % mem_size];
+}
+
+//---------------------------------------------------------//
+//                         ‘”Õ ÷»»                         //
+//---------------------------------------------------------//
+
+template <class T>
+void TQueue<T>::push(T element)
+{
+	if (isFull()) throw - 1;
+	tail = tail % mem_size;
+	pMem[++tail] = element;
+	element_counter++;
+}
+
+template <class T>
+T TQueue<T>::pop()
+{
+	if (isEmpty()) throw - 1;
+	T result = pMem[head];
+	head++;
+	head = head % mem_size;
+	element_counter--;
+	return result;
+}
+
+template<class T>
+bool TQueue<T>::isFull() const {
+	return element_counter == mem_size;
+}
+
+template<class T>
+bool TQueue<T>::isEmpty() const {
+	return element_counter == 0;
+}
+
+template<class T>
+void TQueue<T>::clear()
+{
+	head = 0;
+	tail = -1;
+	element_counter = 0;
+}
+
+template<class T>
+T TQueue<T>::front() const
+{
+	if (isEmpty()) throw - 1;
+	return pMem[head];
+}
+
+template<class T>
+T TQueue<T>::back() const
+{
+	if (isEmpty()) throw - 1;
+	return pMem[tail];
+}
---
+++ Test_stack/test_stack.cpp
@@ -0,0 +1,114 @@
+#include "../Queue/TQueue.h"
+
+#include "gtest.h"
+
+TEST(TQueue, can_create_queue_with_positive_size){
+    ASSERT_NO_THROW(TQueue<int> m(5));
+}
+
+TEST(TQueue, throws_when_create_queue_with_negative_size) {
+    ASSERT_ANY_THROW(TQueue<int> queue(-5));
+}
+
+TEST(TQueue, can_create_copied_queue) {
+    TQueue<int> m(5);
+    ASSERT_NO_THROW(TQueue<int> m1(m));
+}
+
+TEST(TQueue, copied_queue_is_equal_to_source_one) {
+    TQueue<int> m(5);
+    TQueue<int> m1(m);
+    EXPECT_EQ(m, m1);
+}
+
+TEST(TQueue, empty_queue) {
+    TQueue<int> m(5);
+    ASSERT_NO_THROW(m.isEmpty() == true);
+}
+
+TEST(TQueue, no_empty_queue) {
+    TQueue<int> m(5);
+    m.push(1);
+    ASSERT_NO_THROW(m.isEmpty() == false);
+}
+
+TEST(TQueue, full_queue) {
+    TQueue<int> m(2);
+    m.push(5);
+    m.push(2);
+    ASSERT_NO_THROW(m.isFull() == true);
+}
+
+TEST(TQueue, no_full_queue) {
+    TQueue<int> m(5);
+    m.push(5);
+    m.push(10);
+    ASSERT_NO_THROW(m.isFull() == false);
+}
+
+TEST(TQueue, front_function) {
+    TQueue<int> m(3);
+    m.push(2);
+    m.push(3);
+    ASSERT_NO_THROW(m.front() == 2);  // Проверяем первый элемент
+}
+
+TEST(TQueue, back_function) {
+    TQueue<int> m(3);
+    m.push(2);
+    m.push(3);
+    ASSERT_NO_THROW(m.back() == 3);  // Проверяем последний элемент
+}
+
+TEST(TQueue, clear_stack) {
+    TQueue<int>m(2);
+    m.push(5);
+    m.push(2);
+    m.clear();
+    ASSERT_NO_THROW(m.isEmpty() == true);
+}
+
+TEST(TQueue, push_function) {
+    TQueue<int> m(2);
+    m.push(2);
+    m.push(3);
+    ASSERT_NO_THROW(m.back() == 3);
+}
+
+TEST(TQueue, pop_function) {
+    TQueue<int> m(2);
+    m.push(2);
+    m.push(3);
+    m.pop();
+    ASSERT_NO_THROW(m.front() == 3);
+}
+
+TEST(TQueue, pop_from_empty_queue_throws_exception) {
+    TQueue<int> m(3);
+    ASSERT_ANY_THROW(m.pop());  
+}
+
+TEST(TQueue, push_to_full_queue_throws_exception) {
+    TQueue<int> m(2);
+    m.push(1);
+    m.push(2);
+    ASSERT_ANY_THROW(m.push(3));
+}
+
+TEST(TQueue, compare_queues) {
+    TQueue<int> m(2);
+    m.push(5);
+    m.push(2);
+    TQueue<int> m1(m);
+    ASSERT_NO_THROW(m == m1);
+}
+
+TEST(TQueue, no_compare_queue) {
+    TQueue<int> m(2);
+    TQueue<int> m1(m);
+    m.push(5);
+    m.push(10);
+    m1.push(10);
+    m1.push(5);
+    ASSERT_NO_THROW(m != m1);
+}
